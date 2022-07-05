c21f228f9f4ccf51ec234bad150564ec612b6869
Lost <93594637+ItzLost@users.noreply.github.com>
2022-07-05 10:10:35 +0200

    Update Auto broly message.lua

--- Auto broly message.lua
+++ Auto broly message.lua
@@ -1,2 +1,14 @@
 _G.LostName = "Lost "
 _G.NiceMessage= "Love the weather today!"
+
+local args = {
+  [1] = _G.LostName,
+  [2] = "All"
+}
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait(2)
+local args = {
+  [1] = _G.NiceMessage,
+  [2] = "All"
+}
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
