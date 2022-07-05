566b5a2c28e480e69f136d0a21fe93c83f286ee9
Lost <93594637+ItzLost@users.noreply.github.com>
2022-07-05 09:40:37 +0200

    Update List of my commands.lua

--- List of my commands.lua
+++ List of my commands.lua
@@ -44,4 +44,5 @@ local listcmds = {
     print("/lock, it's like an aimbot, it aims the head of the player, PRESS V TO ENABLE/DISABLE")
     print("/sb or /spambeans, well it spams beans/jars, put the cmd again to stop it")
     print("/notm or noteamdamage, in broly you can hit other players")
+  return(listcmds)
 }
