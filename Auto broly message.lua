09fa64e9e7528ce2a275f0ce33ea66044c147d6d
Lost <93594637+ItzLost@users.noreply.github.com>
2024-07-16 11:58:52 +0200

    Update Auto broly message.lua

--- Auto broly message.lua
+++ Auto broly message.lua
@@ -1,8 +1,8 @@
 local LostName = 'ds: itslxst'
 local NiceMessage = 'yessir'
 
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer('This Auto was made by Lost.', 'All')
+game:GetService('ReplicatedStorage').DefaultChatSystemChatEvents.SayMessageRequest:FireServer('This Auto was made by Lost.', 'All')
 task.wait(2)
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(lostName, 'All')
+game:GetService('ReplicatedStorage').DefaultChatSystemChatEvents.SayMessageRequest:FireServer(lostName, 'All')
 task.wait(2)
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(niceMessage, 'All')
+game:GetService('ReplicatedStorage').DefaultChatSystemChatEvents.SayMessageRequest:FireServer(niceMessage, 'All')
